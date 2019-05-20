4e3819588d7c7257b11b876fdaefdd53053d281e
Pablo Nevares <pablo@nevares.com>
2019-05-19 23:39:44 -0700

    Updates

---
+++ precache-manifest.6cd6eae7617f69366e0749b78c6fc1db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be74174fd07fa619ad9a38a763feed6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80549b01278b8c983ff2",
+    "url": "/static/css/main.6435ab3b.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "80549b01278b8c983ff2",
+    "url": "/static/js/main.fdcdefd7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
